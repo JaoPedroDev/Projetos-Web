dfd9a30ee1ec09b8a321a0a0aedc3b520bfdd43a
João Pedro Souza da Cruz <joaopedropessoal.15@gmail.com>
2020-12-21 18:01:14 -0300

    ler-rapido 1.4
    Funcional, design minimo terminado, diniamicamente muda a centração do conteúdo!

--- ler-rapido/script.js
+++ ler-rapido/script.js
@@ -1,14 +1,14 @@
-var ini = document.getElementById('ini')
-var pau = document.getElementById('pau')
-var res = document.getElementById('res')
 var texto = document.getElementById("texto")
 var visor = document.getElementById("visor")
-var textoSeparado = separaPalavras(texto.innerText, ' ')
+var velocidade = document.getElementById("vel")
+var vel = 1000
+var textoSeparado
 var tmp
 var pos = 0
+var i = 0 //Previne que o contador ligue mais de uma vez
 function principal(){
+    textoSeparado = separaPalavras(texto.innerText, ' ')
     if(pos < textoSeparado.length){
-        console.log(tmp)
         visor.innerText = textoSeparado[pos]
         pos++
     }else{
@@ -21,18 +21,36 @@ function separaPalavras(string, separador){
     return arrayDePalavras
 }
 function iniciar(){
-    tmp = setInterval(principal, 1000)
+    if(texto.innerText !== ""){
+        if(i == 0){
+            tmp = setInterval(principal, vel)
+        }
+        i = 1
+    }
 }
 function pausar(){
     clearInterval(tmp)
+    i = 0
 }
 function resetar(){
     pos = 0
     visor.innerText = textoSeparado[0]
+    i = 0
+    clearInterval(tmp)
 }
+function mudar(){
+    vel = velocidade.value
+    pausar()
+    iniciar()
+}
+var ini = document.getElementById('ini')
+var pau = document.getElementById('pau')
+var res = document.getElementById('res')
+var mud = document.getElementById("mud")
 function carregar(){
     ini.addEventListener("click", iniciar)
     pau.addEventListener("click", pausar)
     res.addEventListener("click", resetar)
+    mud.addEventListener("click", mudar)
 }
 window.addEventListener("load", carregar)
