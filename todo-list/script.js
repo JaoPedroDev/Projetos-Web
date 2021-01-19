d85eec1ae83cca80b25e91cb19d7a7d588dc8dc8
João Pedro Souza da Cruz <joaopedropessoal.15@gmail.com>
2021-01-19 15:55:29 -0300

    Alpha
    aa

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,11 +1,14 @@
 var tdList = document.getElementById("tdList")
-var n = 1
+var n = 1 // Usado para colocar o id de cada item da lista como um numero em ordem crescente começando do 1
+
+// Cria um checkbox no HTML para cada item da lista
 function addOnList(){
     tdList.innerHTML += 
     `<li id="${n}"><input type="checkbox" class="cb">${document.getElementById("txtInput").value}</li>`
     n++
 }
 
+// EvenListeners
 var addB = document.getElementById("addB")
 function load(){
     addB.addEventListener("click", addOnList)
