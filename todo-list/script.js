495b0ef89d3b04a05a1e651ae5f6c3e6abb08174
João Pedro Souza da Cruz <joaopedropessoal.15@gmail.com>
2021-01-27 17:47:35 -0300

    Using checkbox(Unusable)

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,15 +1,46 @@
-var tdList = document.getElementById("tdList")
-var n = 1 // Usado para colocar o id de cada item da lista como um numero em ordem crescente começando do 1
+var tdList = document.getElementById("tdList");
+var txtInput = document.getElementById("txtInput");
+var n = 1; // Usado para colocar o id de cada item da lista como um numero em ordem crescente começando do 1
 
 // Cria um checkbox no HTML para cada item da lista
 function addOnList(){
-    tdList.innerHTML += 
-    `<li id="${n}"><input type="checkbox" class="cb">${document.getElementById("txtInput").value}</li>`
-    n++
-}
+    //Cria o elemento
+    var checkbox = document.createElement("input");
+    //Adiciona as informações do elemento
+    checkbox.type = "checkbox";
+    checkbox.id = `cb${n}`;
+    checkbox.name = "tdItem";
+
+    var label = document.createElement("label");
+    label.htmlFor = `cb${n}`;
+    label.id = `lb${n}`;
+    //Adiciona texto dentro do elemento criado
+    label.appendChild(document.createTextNode(txtInput.value));
+
+    var br = document.createElement("br");
+
+    //Adiciona os elementos criados dentro da div "tdList"
+    tdList.appendChild(checkbox);
+    tdList.appendChild(label);
+    tdList.appendChild(br);
+    //Adiciona 1 em "n" para os próximos ids
+    n++;
+};
+
+function isChecked(){
+    if(this.checked){
+        console.log("checkbox is checked...");
+    }
+    else{
+        console.log("checkbox is not checked...");
+    };
+};
 
 // EvenListeners
-var addB = document.getElementById("addB")
-function load(){
-    addB.addEventListener("click", addOnList)
-}
+function main(){
+    var addB = document.getElementById("addB");
+    var cbListener = document.getElementById(`cb${n}`)
+
+    addB.addEventListener("click", addOnList);
+    cbListener.addEventListener("change", isChecked())
+};
